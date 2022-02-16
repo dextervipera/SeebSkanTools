1b6235871f1ea360261f7f4468c972e558a7fb10
KKK <dextervipera@gmail.com>
2022-02-16 14:28:52 +0100

    auto process catalog added

---
+++ R/AutoResults.R
@@ -0,0 +1,10 @@
+cat('\014')
+rm(list = ls())
+
+source("fSSMapGenerate.R")
+
+flist = list.files(path='results/', pattern="*.csv", all.files=FALSE, full.names=T)
+
+for (fpath in flist){
+SSMapGenerate(path = fpath, n=5, csv = TRUE, plots = TRUE, plots.interpolate = FALSE)
+}
