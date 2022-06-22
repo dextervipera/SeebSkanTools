0d4c6b7967dec2d0543f4baadc773635c0a4946a
KKK <dextervipera@gmail.com>
2022-06-22 08:03:13 +0200

    hedimat seire 02

--- R/AutoResults.R
+++ R/AutoResults.R
@@ -8,3 +8,4 @@ flist = list.files(path='results/', pattern="*.csv", all.files=FALSE, full.names
 for (fpath in flist){
 SSMapGenerate(path = fpath, n=5, csv = TRUE, plots = TRUE, plots.interpolate = FALSE)
 }
+
